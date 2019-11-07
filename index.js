2adf95e429d7427953b580ca8096d22e769a64f9
Jose Luis <luizalcantara59@gmail.com>
2019-11-07 01:36:50 -0600

    commit

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ app.use(
 app.use(bodyParser.json());
 
 app.post("/echo", function(req, res) {
-  var speech =
+  /*var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Ocurrió un problema, vato. Habla de nuevo.";
+      : "Ocurrió un problema, vato. Habla de nuevo.";*/
+    if (req.body.queryResult.action == "costo") {
+      let costo = parseFloat(req.body.queryResult.parameters.costo);
+      response = "El costo es de " + costo;
+      res.json({
+          "fulfillmentText": response
+      });
+    }
   
   var speechResponse = {
     google: {
@@ -44,6 +51,18 @@ app.post("/echo", function(req, res) {
     displayText: speech,
     source: "webhook-echo-sample"
   });
+});
+
+app.post('/', function (req, res) {
+  if (req.body.queryResult.action == "suma") {
+      let num1 = parseFloat(req.body.queryResult.parameters.num1);
+      let num2 = parseFloat(req.body.queryResult.parameters.num2);
+      let sum = num1 + num2;
+      response = num1 + " + " + num2 + " es " + sum;
+      res.json({
+          "fulfillmentText": response
+      });
+  }
 });
 
 app.get('/', (req, res) => {
