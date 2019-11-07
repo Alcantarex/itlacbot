13c6db29e5b6489592cfbdf1252894bf0e5a94d0
Alcantarex <32251708+Alcantarex@users.noreply.github.com>
2019-11-07 00:18:33 -0600

    Creation

---
+++ index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+
+app.post("/echo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Ocurrió un problema, vato. Habla de nuevo.";
+  
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
+    }
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+app.get('/', (req, res) => {
+	res.status(200).send('app is working.')
+})
+
+app.listen(port, () => {
+	console.log(`🌏 app is running at http://localhost:${port}`)
+})
+
+app.listen(process.env.PORT || 8000, function() {
+  console.log("app up and listening");
+});
