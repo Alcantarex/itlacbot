852038ae37be76be28b8147033c1331154c678d1
Jose Luis <luizalcantara59@gmail.com>
2019-11-24 21:19:39 -0600

    Incorporacion de nuevos deptos

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post("/echo", function(req, res) {
         "Ingeniería Industrial",
         "Ingeniería en Gestión Empresarial",
         "Contabilidad",
-        "y Administración"
+        "Administración"
       ];
       var aFrases = [
         "En el Instituto Tecnológico de Lázaro Cárdenas, hay 6 Ingenierías y 2 Licenciaturas, y son",
@@ -60,8 +60,16 @@ app.post("/echo", function(req, res) {
       var sCarreras = "";
       var iTotalCarreras = aCarreras.length;
       for(var i = 0; i < iTotalCarreras; i++){
+        if(i = iTotalCarreras - 1)
+          sCarreras = sCarreras + "y \n" + aCarreras[i];
+        else 
+          sCarreras = sCarreras + ", \n" + aCarreras[i];
+      }
+      /*
+      for(var i = 0; i < iTotalCarreras; i++){
         sCarreras = sCarreras + "\n" + aCarreras[i];
       }
+      */
       speech = aFrases[ Math.round(Math.random()*aFrases.length) ] + "\n" + sCarreras;
     //COSTO --------------------------------------------------
     } else if (req.body.queryResult.action == "costo") {
@@ -73,6 +81,48 @@ app.post("/echo", function(req, res) {
         "Actualmente, en el " + dFecha.getFullYear() + " la inscripción tiene un costo de $" + cCosto,
         "Mira, el semeste de inscripción tiene un costo de $" + cCosto + ", a partir de ahí, cada semestre siguiente se va reduciendo cada $100"];
       speech = aCosto[ Math.round(Math.random()*aCosto.length) ];
+    //DEPARTAMENTO --------------------------------------------------
+    } else if (req.body.queryResult.action == "departamento") {
+      var aDeptos = [
+        "Dirección",
+        "Servicios Escolares",
+        "Recursos Financieros",
+        "Extraescolares",
+        "Comunicación y difusión",
+        "Gestión Tecnológica y Vinculación",
+        "Titulación",
+        "Ciencias Básicas",
+        "Recursos Materiales",
+        "División de Estudios",
+        "Centro de cómputo"
+      ];
+      var aFuncion = [
+        "Dirigir y gestionar de manera general el plantel.",
+        "Planear, coordinar, controlar y evaluar las actividades relacionadas con la prestación de los servicios escolares a los alumnos del Instituto Tecnológico, conforme a las normas y lineamientos por la Secretaria de Educación Pública.",
+        "Planear, coordinar, controlar y evaluar las actividades relacionadas con la administración de los recursos financieros del Instituto Tecnológico conforme a las normas y lineamientos establecidos por la Secretaría de Educación Pública.",
+        "Gestionar todas aquellas actividades tales como basquetbol, futbol, danza, ajedrez, entre otras; y suelen desarrollarse al mediodía o por la tarde, una vez finalizada las clases",
+        "Comunicar eventos, activiades o acontecimientos relacionados con el plantel, a través de los medios de comunicación de la ciudad.",
+        "Planear, coordinar, controlar y evaluar las actividades relacionadas con el servicio social, las residencias profesionales, la bolsa de trabajo e inglés.",
+        "Planear, coordinar, controlar y evaluar las actividades relacionadas con el proceso de titulación de los estudiantes en sus respectivas opciones.",
+        "Desarrollar competencias en el plantel relacionadas con las ciencias tales como matemáticas, física o química.",
+        "Administrar y gestionar las herramientas y materiales del plantel necesarios para realizar actividades afines.",
+        "Gestionar aspectos académicos de los estudiantes tales como las altas, bajas, exámenes especiales, plan de estudios, etcétera.",
+        "Propiciar que las tecnologías lleguen a todos los usuarios, siendo instrumento para sus investigaciones, ejercicio docente o en su defecto herramientas fundamentales para el desarrollo educativo."
+      ];
+      if (req.body.queryResult.parameters.parameters == "cuales") {
+        
+        speech = "Los departamentos del itlac son ";
+        for(var i = 0; i < aDeptos.length; i++){
+          speech = speech + "\n" + aDeptos[i];
+        }
+
+      } else if (req.body.queryResult.parameters == "funcion") {
+        var sDepto = req.body.queryResult.parameters.depto;
+        aDeptos.indexOf(sDepto); 
+        speech = "";
+      }
+      
+      
     //ERROR --------------------------------------------------
     } else {
       speech = "Ocurrió un problema. Hable de nuevo, por favor.";
@@ -102,6 +152,19 @@ app.post("/echo", function(req, res) {
     source: "webhook-echo-sample"
   });
 });
+
+
+/*app.post('/', function (req, res) {
+  if (req.body.queryResult.action == "suma") {
+      let num1 = parseFloat(req.body.queryResult.parameters.num1);
+      let num2 = parseFloat(req.body.queryResult.parameters.num2);
+      let sum = num1 + num2;
+      response = num1 + " + " + num2 + " es " + sum;
+      res.json({
+          "fulfillmentText": response
+      });
+  }
+});*/
 
 app.get('/', (req, res) => {
   var html = 
